0fb5e73ccffe410de867a9b6c1622b2d98ef00cf
chenxeed <chenxeed@gmail.com>
2020-04-20 00:45:30 +0800

    build: add vue-next

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+createApp(App)
+  .use(router)
+  .use(store)
+  .mount("#app");
